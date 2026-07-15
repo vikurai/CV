841a530eba8497b7e40f37a93b93693fff2d5f09
Vinay Kumar Rai <vikurai20@gmail.com>
2026-07-15 14:12:24 +0530

    added projects

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -17,7 +17,7 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid white' }}
-            date="Jan 2026 – Present"
+            date="Jan 2026 – May 2026"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
